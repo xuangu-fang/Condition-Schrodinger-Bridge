3c4dd0137c3e855776a6647abe5aa6d99c5a702f
Wei Deng <greyman@live.cn>
2022-07-04 23:59:34 -0400

    1000 data points for spiral

--- options.py
+++ options.py
@@ -27,7 +27,7 @@ def get_default_configs():
 
     # sampling
     #config.samp_bs = 1000 # cantor server doesn't support large batch size
-    config.samp_bs = 200
+    config.samp_bs = 800
     config.sigma_min = 0.01
     config.sigma_max = 0.5
 
--- test_wei.sh
+++ test_wei.sh
@@ -1,5 +1,5 @@
-python main.py --problem-name Scurve --forward-net toy --backward-net toy  --dir ./ 
+#python main.py --problem-name Scurve --forward-net toy --backward-net toy  --dir ./ 
 python main.py --problem-name Spiral --forward-net toy --backward-net toy  --dir ./
-python main.py --problem-name Moon --forward-net toy --backward-net toy  --dir ./
-python main.py --problem-name Circle --forward-net toy --backward-net toy  --dir ./
-python main.py --problem-name Pinwheel --forward-net toy --backward-net toy  --dir ./
+#python main.py --problem-name Moon --forward-net toy --backward-net toy  --dir ./
+#python main.py --problem-name Circle --forward-net toy --backward-net toy  --dir ./
+#python main.py --problem-name Pinwheel --forward-net toy --backward-net toy  --dir ./
